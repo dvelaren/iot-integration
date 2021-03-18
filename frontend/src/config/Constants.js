74a0dd4dc1dfe047956adf2bcc231c7120834daa
David Velasquez <tidusdavid@gmail.com>
2021-03-17 19:58:21 -0500

    Refactor colors

--- frontend/src/config/Constants.js
+++ frontend/src/config/Constants.js
@@ -4,4 +4,9 @@ export const backendHost = {
   data: host.concat('data')
 };
 
+export const varColors = {
+  temperature: '#F50057',
+  humidity: '#17BECF'
+}
+
 export const refreshInterval = 1000;
